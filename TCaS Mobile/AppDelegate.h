3b613e3102e272b2f6e8fb1fd285ef53e03683bf
Kyle Zhou <kylezhou2002@hotmail.com>
2016-01-04 19:21:38 -0800

    Initial Commit

---
+++ TCaS Mobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCaS Mobile
+//
+//  Created by Kyle Zhou on 2016-01-04.
+//  Copyright © 2016 Kyle Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCaS MobileTests/TCaS_MobileTests.m
@@ -0,0 +1,39 @@
+//
+//  TCaS_MobileTests.m
+//  TCaS MobileTests
+//
+//  Created by Kyle Zhou on 2016-01-04.
+//  Copyright © 2016 Kyle Zhou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TCaS_MobileTests : XCTestCase
+
+@end
+
+@implementation TCaS_MobileTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
