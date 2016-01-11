138ac355336b7766d21761f28f7cdf4977c242e1
Kyle Zhou <kylezhou2002@hotmail.com>
2016-01-11 13:27:37 -0500

    Removed MasterDetailView

---
+++ TCaS Mobile/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  TCaS Mobile
+//
+//  Created by Kyle Zhou on 2016-01-06.
+//  Copyright © 2016 Kyle Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
