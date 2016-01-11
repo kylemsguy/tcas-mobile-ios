d3e0d4c93e8bdfedefe3e295bef5a62ca8d337fa
Kyle Zhou <kylezhou2002@hotmail.com>
2016-01-11 13:45:40 -0500

    Starting to write TCaSSession interface

---
+++ TCaS Mobile/TCaSSession.h
@@ -0,0 +1,17 @@
+//
+//  TCaSSession.h
+//  TCaS Mobile
+//
+//  Created by Kyle Zhou on 2016-01-11.
+//  Copyright © 2016 Kyle Zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCaSSession : NSObject
+
++ (NSString *)getPageFromUrl: (NSString *) url withParams: (NSString *)params;
++ (NSString *)sendUrlPostToUrl: (NSString *) url withParams: (NSString *)params;
+//(NSString *)sendMultipartPostToUrl: (NSString *) url withParams: (NSArray *) array;
+
+@end
---
+++ TCaS Mobile/TCaSSession.m
@@ -0,0 +1,13 @@
+//
+//  TCaSSession.m
+//  TCaS Mobile
+//
+//  Created by Kyle Zhou on 2016-01-11.
+//  Copyright © 2016 Kyle Zhou. All rights reserved.
+//
+
+#import "TCaSSession.h"
+
+@implementation TCaSSession
+
+@end
