f5a891b825f344b04d6e224757b2adb88c53ea06
Kyle Zhou <kylezhou2002@hotmail.com>
2016-01-12 13:55:45 -0500

    trying to implement HTTP request

--- TCaS Mobile/TCaSConnection.m
+++ TCaS Mobile/TCaSConnection.m
@@ -21,11 +21,32 @@
     return self;
 }
 
-+ (NSString *)getPageFromUrl:(NSString *)url withParams:(TCaS_Backend::Request *)params {
++ (NSString *)getPageFromUrl:(NSString *)url withParams:NSDictionary params {
     NSURL *urlObj = [NSURL URLWithString:url];
 
     // implement rest
+    NSMutableURLRequest request = [NSMutableURLRequest requestWithURL: urlObj];
+    request.HTTPMethod = @"GET";
+    // This is how we set header fields
+    [request setValue:@"application/x-www-form-urlencoded; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
 
+    if (params) {
+      NSData *jsonData = [NSJSONSerialization dataWithJSONObject: params options:0 error:nil];
+      NSString *urlString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+      NSString *stringData = [[NSString alloc] initWithFormat:@"jsonRequest=%@", urlString];
+      NSData *requestBodyData = [stringData dataUsingEncoding:NSUTF8StringEncoding];
+      request.HTTPBody = requestBodyData;
+    }
+
+    NSURLConnection *theConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+
+    if (!theConnection) {
+        // Release the receivedData object.
+        NSMutableData *responseData = nil;
+
+        // Inform the user that the connection failed.
+        return nil;
+    }
     return @"";
 }
 
@@ -33,6 +54,9 @@
     NSURL *urlObj = [NSURL URLWithString:url];
 
     // implement rest
+    NSMutableURLRequest request = [NSMutableURLRequest requestWithURL: urlObj];
+    // This is how we set header fields
+    [request setValue:@"application/x-www-form-urlencoded; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
 
     return @"";
 }
